be5624a34ac6921af14e0a5be7b10d4739dfebad
Ana Francisca <up202307150@up.pt>
2024-10-10 12:00:49 +0100

    Using Lanterna

--- src/main/java/com/up202307150/Application.java
+++ src/main/java/com/up202307150/Application.java
@@ -1,7 +1,27 @@
 package com.up202307150;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args ) {
-        System.out.println("Hello World");
+    public static void main(String[] args)  throws IOException{
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new
+                DefaultTerminalFactory()
+                .setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+        screen.clear();
+        screen.setCharacter(10, 10, new TextCharacter ('X'));
+        screen.refresh();
     }
 }
