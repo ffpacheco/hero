9144a5dbea9aa297552d48d18217dd9048267c99
Claudio Meireles <up202306618@up.pt>
2024-10-11 13:33:20 +0100

    Fix Screen Null Error

--- src/main/java/com/up202307150/Application.java
+++ src/main/java/com/up202307150/Application.java
@@ -1,10 +1,9 @@
 package com.up202307150;
 
-
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args)  throws IOException{
+    public static void main(String[] args) throws IOException {
         Game game = new Game();
         game.run();
     }
