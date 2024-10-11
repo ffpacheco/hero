4c06b2b17fe648c6fdb38725942c7aca36f3ca1d
Ana Francisca <up202307150@up.pt>
2024-10-11 22:55:02 +0100

    screen null fix

--- src/main/java/com/up202307150/Game.java
+++ src/main/java/com/up202307150/Game.java
@@ -9,8 +9,8 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
-public class Game  {
-    private Screen screen;
+public class Game {
+    private final Screen screen;
 
     public Game() throws java.io.IOException {
         TerminalSize terminalSize = new TerminalSize(40, 20);
@@ -20,19 +20,20 @@ public class Game  {
                 .setInitialTerminalSize(terminalSize);
 
         Terminal terminal = terminalFactory.createTerminal();
-        Screen screen = new TerminalScreen(terminal);
+        this.screen = new TerminalScreen(terminal);
 
-        screen.setCursorPosition(null);
-        screen.startScreen();
-        screen.doResizeIfNecessary();
+        this.screen.setCursorPosition(null);
+        this.screen.startScreen();
+        this.screen.doResizeIfNecessary();
     }
+
     private void draw() throws IOException {
-
-        screen.clear();
-        screen.setCharacter(10, 10, new TextCharacter ('X'));
-        screen.refresh();
+        this.screen.clear();
+        this.screen.setCharacter(10, 10, new TextCharacter('X'));
+        this.screen.refresh();
     }
+
     public void run() throws IOException {
-        draw();
+        this.draw();
     }
 }
