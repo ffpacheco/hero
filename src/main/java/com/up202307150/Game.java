086ec950858df9ccfb8b5bba7b6fcc9013fd43b4
Ana Francisca <up202307150@up.pt>
2024-10-11 23:57:53 +0100

    Reading KeyStrokes

--- src/main/java/com/up202307150/Game.java
+++ src/main/java/com/up202307150/Game.java
@@ -2,15 +2,20 @@ package com.up202307150;
 
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+
 import java.io.IOException;
 
-public class Game {
+public class Game  {
     private final Screen screen;
+    private int x=10;
+    private int y=10;
+    private boolean running = true;
 
     public Game() throws java.io.IOException {
         TerminalSize terminalSize = new TerminalSize(40, 20);
@@ -20,20 +25,61 @@ public class Game {
                 .setInitialTerminalSize(terminalSize);
 
         Terminal terminal = terminalFactory.createTerminal();
-        this.screen = new TerminalScreen(terminal);
+        screen = new TerminalScreen(terminal);
 
-        this.screen.setCursorPosition(null);
-        this.screen.startScreen();
-        this.screen.doResizeIfNecessary();
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+    }
+    private void processKey(KeyStroke key) throws IOException {
+        switch (key.getKeyType()) {
+            case EOF:
+                running =false;
+                break;
+
+            case ArrowDown:
+                y++;
+                break;
+
+            case ArrowUp:
+                y--;
+                break;
+
+            case ArrowRight:
+                x++;
+                break;
+
+            case ArrowLeft:
+                x--;
+                break;
+
+            case Character:
+                readChar(key.getCharacter());
+                break;
+        }
+    }
+
+    private void readChar(char c) throws IOException {
+        if (c == 'q' || c == 'Q'){
+            screen.close();
+            running=false;
+        }
     }
 
     private void draw() throws IOException {
-        this.screen.clear();
-        this.screen.setCharacter(10, 10, new TextCharacter('X'));
-        this.screen.refresh();
+
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter ('X')[0]);
+        screen.refresh();
     }
 
     public void run() throws IOException {
-        this.draw();
+        running = true;
+        while(running) {
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+        }
+
     }
 }
